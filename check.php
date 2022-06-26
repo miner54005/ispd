de9c4238349f0c1f8cea89898f4777177ce8bd11
miner54005 <82769775+miner54005@users.noreply.github.com>
2022-06-27 02:44:42 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,4 +1,12 @@
-<?php
-	$login = $_POST['login'];
-	echo login;
+<?php 
+	$fname = filter_var(trim($_POST['fname']), FILTER_SANITIZE_STRING);
+	$name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+	$sname = filter_var(trim($_POST['sname']), FILTER_SANITIZE_STRING);
+	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
+	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+
+
+	$mysql = mysqli_connect('localhost', 'root', 'root', 'rse_db');
+	$mysql->query("INSERT INTO `info_client` (`fname`, `name`, `sname`, `login`, `pass`) VALUES ('$fname', '$name', '$sname', '$login', '$pass')");
+	$mysql->close();
 ?>
