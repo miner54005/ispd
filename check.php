79d92a9b9a6bd4bef4d9735e7f504db3837f891f
miner54005 <82769775+miner54005@users.noreply.github.com>
2022-06-26 23:00:05 +0300

    Test php

--- check.php
+++ check.php
@@ -1,12 +1,4 @@
 <?php
-	$fname = filter_var(trim($_POST['FName']), FILTER_SANITIZE_STRING);
-	$name = filter_var(trim($_POST['Name']), FILTER_SANITIZE_STRING);
-	$sname = filter_var(trim($_POST['SName']), FILTER_SANITIZE_STRING);
-	$login = filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
-	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
-
-	if(mb_strlen($login) <2 || mb_strlen($login) >8) {
-		echo "Недопустима довжина логіна";
-		exit();
-	}
+	$login = $_POST['login'];
+	echo $login;
 ?>
